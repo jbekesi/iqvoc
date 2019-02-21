d271985bbeb28f4b01af79bfc3e1fc6624c4a259
Janos Bekesi <janos.bekesi@univie.ac.at>
2019-02-21 15:15:42 +0100

    changed version display 5

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'iqvoc'
+# require 'iqvoc'
 require 'iqvoc/phdr/version'
 
 Iqvoc.host_namespace = Iqvoc::Phdr
